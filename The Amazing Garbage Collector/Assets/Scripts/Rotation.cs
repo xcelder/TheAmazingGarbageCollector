7adf450f08b3183dfbaada3f70a5cb7649ebcedd
Víctor Álvarez <victor.alvrom@gmail.com>
2019-10-19 11:26:49 +0200

    Create Unity Project

---
+++ The Amazing Garbage Collector/Assets/Scripts/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private float degreesPerSecond;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, degreesPerSecond * Time.deltaTime);
+    }
+}
