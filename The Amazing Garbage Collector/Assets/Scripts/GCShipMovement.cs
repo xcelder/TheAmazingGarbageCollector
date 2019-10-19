635cb5f9486d86007d748660518771e30213ac5a
Unknown <xcelder@gmail.com>
2019-10-19 16:48:17 +0200

    Movement script

---
+++ The Amazing Garbage Collector/Assets/Scripts/GCShipMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GCShipMovement : MonoBehaviour
+{
+
+    public float speedH = 1.0f;
+    public float speedV = 1.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    private int acceleration = 1;
+
+    private int currentSpeed = 0;
+    private float currentShipSpeed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCameraPosition();
+
+        if ((Input.GetKey("up") || Input.GetKey("w")))
+        {
+            currentSpeed += acceleration;
+        }
+        else if ((Input.GetKey("down") || Input.GetKey("s")))
+        {
+            currentSpeed -= acceleration;
+        }
+
+        currentShipSpeed = Mathf.Lerp(currentShipSpeed, currentSpeed, Time.deltaTime * acceleration);
+
+        transform.position += transform.TransformDirection(Vector3.down) * currentShipSpeed * Time.deltaTime;
+    }
+
+    void UpdateCameraPosition()
+    {
+        yaw -= speedH * Input.GetAxis("Mouse X");
+        pitch += speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, 0.0f, yaw);
+    }
+}
