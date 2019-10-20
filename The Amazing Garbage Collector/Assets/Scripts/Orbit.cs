2c1b1a1674d75cf3e15c68739540d52955f36f9d
Víctor Álvarez <Eclipse_Storm_@hotmail.com>
2019-10-20 08:40:13 +0200

    Add a speed debugger in the Orbit script

--- The Amazing Garbage Collector/Assets/Scripts/Orbit.cs
+++ The Amazing Garbage Collector/Assets/Scripts/Orbit.cs
@@ -7,10 +7,18 @@ public class Orbit : MonoBehaviour
     [SerializeField] private Vector3 orbitAxis;
     [SerializeField] private float revsPerSecond;
 
+    [SerializeField] private bool debug;
+
 
     void Update()
     {
+        Vector3 prevPosition = transform.position;
         transform.RotateAround(new Vector3(), orbitAxis, revsPerSecond / 360f * Time.deltaTime);
+        if (debug) {
+            Vector3 newPosition = transform.position;
+            float distance = Vector3.Distance(newPosition, prevPosition);
+            Debug.LogFormat("{0} moved a distance of {1} units. Speed: {2} u/s.", gameObject.name, distance, distance / Time.deltaTime);
+        }
     }
 
 
